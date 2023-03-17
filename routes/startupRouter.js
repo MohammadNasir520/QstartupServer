011d62c7209dc2118493bc302aef8258637c0ff6
MohammadNasir520 <nasirahsan520@gmail.com>
2023-03-17 19:25:18 +0600

    sturtup find by id and all

--- routes/startupRouter.js
+++ routes/startupRouter.js
@@ -1,8 +1,22 @@
+
 const express = require("express")
+const { ObjectId } = require("mongodb")
 const router = express.Router()
+const user = require('../models/userModels')
 
 router.get("/admin/getAllStartUp", async (req, res) => {
     console.log('startUp route hit')
-    res.json('startup route hit')
+    const startUpId = req.query.startUpId;
+    let query = {
+        role: 'startUp'
+    }
+    if (startUpId) {
+        query = {
+            role: 'startUp',
+            _id: new ObjectId(startUpId)
+        }
+    }
+    const allUser = await user.find(query)
+    res.json(allUser)
 })
 module.exports = router;
