1cee397bafc2626c4ea9f900e8eb7cdf3009cc5b
MohammadNasir520 <nasirahsan520@gmail.com>
2023-04-09 03:35:35 +0600

    cors *

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ const corsOptions = {
 };
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5502');
+    res.header('Access-Control-Allow-Origin', '*');
     next();
 });
 
