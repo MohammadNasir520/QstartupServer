73d5321533b0a05bf22192227d263f19e9c5d45a
MohammadNasir520 <nasirahsan520@gmail.com>
2023-04-09 06:01:02 +0600

    credintial added

--- index.js
+++ index.js
@@ -70,6 +70,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
     next();
 });
 
