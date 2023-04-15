b9ad1f836860d3f6339f0f8e9cac382c52ab287a
MohammadNasir520 <nasirahsan520@gmail.com>
2023-04-15 06:01:21 +0600

    domain added

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.use(startUpRouter)
 
 // cors bolck handle
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5502');
+    res.setHeader('Access-Control-Allow-Origin', 'https://deft-custard-35113f.netlify.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
